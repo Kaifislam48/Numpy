5122d6dfc2e791d00e19581886b83322d595ed2d
Kaif Islam <117916990+Kaifislam48@users.noreply.github.com>
2024-02-22 11:38:41 +0530

    Add files via upload

---
+++ CreateNumpy.py
@@ -0,0 +1,15 @@
+#NumPy is a Python library used for working with arrays.
+
+import numpy as np
+arr1 = np.array([1, 2, 3, 4, 5])
+arr2 = np.array([2, 3, 4, 5, 6])
+print(arr1)
+print(arr2)
+sum = arr1+arr2
+sub = arr1-arr2
+mul = arr1*arr2
+div = arr1/arr2
+print(sum)
+print(sub)
+print(mul)
+print(div)
